9b6f3e4e440dd350f28bbae9e28758c4f3d15c4e
Nizam <nizamy710@gmail.com>
2021-08-16 19:06:11 +0530

    feat: added 3 files

---
+++ index.js
@@ -0,0 +1,7 @@
+const billAmount = document.querySelector('#bill-amount');
+const cashGiven = document.querySelector('#cash-given');
+const checkButton = document.querySelector('#check-btn');
+
+const notes = [2000, 500, 100, 20, 10, 5, 1];
+
+console.log(notes);
