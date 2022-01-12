befd1c2bb2ae89cc93c80a88c70798cc94861137
nizam <nizamy710@gmail.com>
2022-01-13 00:16:38 +0530

    code upgraded to es6 with arrow function

--- index.js
+++ index.js
@@ -5,55 +5,61 @@ const checkButton = document.querySelector('#check-btn');
 const errorMessage = document.querySelector('#error-message');
 const noOfNotes = document.querySelectorAll('.no-of-notes');
 
-cashGiven.style.display = 'none';
-checkButton.style.display = 'none';
+const errorType1 =
+    'Error: Input field can not be empty or negative. Please fill correct details.',
+  errorType2 =
+    'Error: do you want to wash plates ? Enter cash amount greater than bill amount.',
+  availableNotes = [2000, 500, 100, 20, 10, 5, 1];
 
-const availableNotes = [2000, 500, 100, 20, 10, 5, 1];
+const hideFunction = (isMsg) => {
+  if (!isMsg) {
+    errorMessage.style.display = 'none';
+  } else {
+    isMsg.style.display = 'none';
+    isMsg.style.display = 'none';
+  }
+};
 
-function validateBillAndCashAmount() {
-  hideMessage();
+hideFunction(cashGiven);
+hideFunction(checkButton);
+
+const showMessage = (msg) => {
+  errorMessage.style.display = 'block';
+  errorMessage.innerText = msg;
+};
+
+const showCashInputField = () => {
+  if (Number(billAmount.value) > 0) {
+    hideFunction();
+    hideFunction(nextButton);
+    checkButton.style.display = 'block';
+    cashGiven.style.display = 'block';
+  } else {
+    showMessage(errorType1);
+  }
+};
+
+const validateBillAndCashAmount = () => {
+  hideFunction();
   if (Number(cashGiven.value) > 0) {
     if (Number(cashGiven.value) >= Number(billAmount.value)) {
       const remainingAmount = cashGiven.value - billAmount.value;
       calculateChange(remainingAmount);
     } else {
-      showMessage(
-        'Error: do you want to wash plates ? Enter cash amount greater than bill amount.'
-      );
+      showMessage(errorType2);
     }
   } else {
-    showMessage(
-      'Error: Input field can not be empty or negative. Please fill correct details.'
-    );
+    showMessage(errorType1);
   }
-}
+};
 
-function calculateChange(remainingAmount) {
+const calculateChange = (remainingAmount) => {
   for (let i = 0; i < availableNotes.length; i++) {
     const NumberOfNotes = Math.trunc(remainingAmount / availableNotes[i]);
     remainingAmount %= availableNotes[i];
     noOfNotes[i].innerText = NumberOfNotes;
   }
-}
+};
 
-function hideMessage() {
-  errorMessage.style.display = 'none';
-}
-
-function showMessage(msg) {
-  errorMessage.style.display = 'block';
-  errorMessage.innerText = msg;
-}
-
-nextButton.addEventListener('click', () => {
-  if (Number(billAmount.value) > 0) {
-    cashGiven.style.display = 'block';
-    checkButton.style.display = 'block';
-    nextButton.style.display = 'none';
-  } else {
-    showMessage(
-      'Error: Input field can not be empty or negative. Please fill correct details.'
-    );
-  }
-});
+nextButton.addEventListener('click', showCashInputField);
 checkButton.addEventListener('click', validateBillAndCashAmount);
